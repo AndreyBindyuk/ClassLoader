7e704ace1b89443be3ac18ed0dd574958234c6b7
Andrey_Bindyuk <Andrey_Bindyuk@epam.com>
2017-03-09 13:16:17 +0600

    fix issues

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,13 +7,18 @@ import java.io.InputStreamReader;
  * Created by Andrey_Bindyuk on 2/27/2017.
  */
 public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws Exception {
+        String input;
         while (true) {
-
-            ClassLoader loader= new CClassLoader(new String[] {"D:\\MENTORINGAPPLICATIONS"});
-            Class clazz= Class.forName("SemaphoreTest",true,loader);
-            Object object= clazz.newInstance();
+            System.out.println("Type directory path: ");
+            input = br.readLine();
+            ClassLoader loader = new CClassLoader(new String[]{input});
+            System.out.println("enter Class name without extension: ");
+            input = br.readLine();
+            Class clazz = Class.forName(input, true, loader);
+            Object object = clazz.newInstance();
             System.out.println(object);
             new BufferedReader(new InputStreamReader(System.in)).readLine();
         }
--- src/main/java/classloader/CClassLoader.java
+++ src/main/java/classloader/CClassLoader.java
@@ -19,7 +19,7 @@ public class CClassLoader extends ClassLoader {
 
     @Override
     protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
-        Class result= findClass(name);
+        Class result = findClass(name);
         if (resolve)
             resolveClass(result);
         return result;
@@ -27,48 +27,46 @@ public class CClassLoader extends ClassLoader {
 
     @Override
     protected Class<?> findClass(String name) throws ClassNotFoundException {
-        Class result = (Class)classesHash.get(name);
-        if (result!=null) {
-            return result;
-        }
-        File f= findFile(name.replace('.','/'),".class");
-        if (f==null) {
+        Class result;
+        File f = findFile(name.replace('.', '/'), ".class");
+        if (f == null) {
             return findSystemClass(name);
         }
-
-        try {
-            byte[] classBytes= loadFileAsBytes(f);
-            result= defineClass(name,classBytes,0,classBytes.length);
-        } catch (IOException e) {
-            throw new ClassNotFoundException("Cannot load class "+name+": "+e);
-        } catch (ClassFormatError e) {
-            throw new ClassNotFoundException("Format of class file incorrect for class "+name+": "+e);
+        result = (Class) classesHash.get(name);
+        if (result != null) {
+            return result;
         }
-        classesHash.put(name,result);
+        try {
+            byte[] classBytes = loadFileAsBytes(f);
+            result = defineClass(name, classBytes, 0, classBytes.length);
+        } catch (IOException e) {
+            throw new ClassNotFoundException("Cannot load class " + name + ": " + e);
+        } catch (ClassFormatError e) {
+            throw new ClassNotFoundException("Format of class file incorrect for class " + name + ": " + e);
+        }
+        classesHash.put(name, result);
         return result;
     }
 
-    private File findFile(String name, String extension)
-    {
-        for (int k=0; k <classPath.length; k++) {
-            File f= new File((new File(classPath[k])).getPath() +
-                    File.separatorChar + name.replace('/',File.separatorChar) + extension);
+    private File findFile(String name, String extension) {
+        for (int k = 0; k < classPath.length; k++) {
+            File f = new File((new File(classPath[k])).getPath() +
+                    File.separatorChar + name.replace('/', File.separatorChar) + extension);
             if (f.exists())
                 return f;
         }
         return null;
     }
 
-    public static byte[] loadFileAsBytes(File file) throws IOException{
-        byte[] result = new byte[(int)file.length()];
+    public static byte[] loadFileAsBytes(File file) throws IOException {
+        byte[] result = new byte[(int) file.length()];
         FileInputStream f = new FileInputStream(file);
         try {
-            f.read(result,0,result.length);
-        }finally {
+            f.read(result, 0, result.length);
+        } finally {
             try {
                 f.close();
-            }
-            catch (Exception e) {
+            } catch (Exception e) {
             }
         }
         return result;
